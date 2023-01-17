379993c854303fd729a3e3bc7f6e43201fdd039e
Noah Rollins <noahjrollins@gmail.com>
2023-01-17 09:43:19 -0600

    commit tree 5d988b35301b1b2d263942df78d1d8a2ab086fd0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT.none? { |bait| bait.match title }
+            errors.add(:title, 'must be clickbait')
+        end
+    end
 end
